0a4519b7c5a90d0c2004f534fb9ffd5790beb006
AlfonsoJBF <129969269+AlfonsoJBF@users.noreply.github.com>
2023-07-13 20:35:34 +0200

    Add files via upload

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -2,16 +2,17 @@ import SkillCard from "./skillCard"
 import HTML from '../assets/SVG/icons8-html-5.svg'
 import JavaScript from '../assets/SVG/icons8-javascript.svg'
 import CSS from '../assets/SVG/icons8-css3.svg'
+import REACT from '../assets/SVG/react.svg'
 
 export default function SkillsSection(){
     return(
-    <section>
+    <section className="skills-container">
         <h1>Pagina de Skills</h1>
         <div className="cards-container">
-            <SkillCard img={HTML} skill='HTML5' progress='80'/>
-            <SkillCard img={CSS} skill='CSS3' progress='80'/>
-            <SkillCard img={JavaScript} skill='JavaScript' progress='80'/>
-            <SkillCard img={HTML} skill='HTML5' progress='80'/>
+            <SkillCard img={HTML} skill='HTML5' progress='100' bgColor='orange'/>
+            <SkillCard img={CSS} skill='CSS3' progress='75' bgColor='blue'/>
+            <SkillCard img={JavaScript} skill='JavaScript' progress='50' bgColor='yellow'/>
+            <SkillCard img={REACT} skill='REACT' progress='10' bgColor='cyan'/>
         </div>
 
     </section>)
